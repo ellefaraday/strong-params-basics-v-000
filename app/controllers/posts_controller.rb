3ceb94771d254af433d857d8dab40bc0c558a2a8
Elle Faraday <faraday.elle@gmail.com>
2018-05-18 21:04:20 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,7 +19,7 @@ class PostsController < ApplicationController
 
 	def update
 	  @post = Post.find(post_params)
-	  @post.update(params.require(:post).permit(:title))
+	  @post.update(post_params)
 	  redirect_to post_path(@post)
 	end
 
